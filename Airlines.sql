7bc72a729d9b7e5635f2a4b7d1efd304ac835230
Shrutika Kadam <shrutikakadam8535@gmail.com>
2025-07-10 21:24:24 +0530

    Add files via upload

---
+++ Airlines.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 10, 2025 at 05:49 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pro-flight_reservation_system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Airlines`
+--
+
+CREATE TABLE `airlines` (
+  `airline_id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `country` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Airlines`
+--
+
+INSERT INTO `Airlines` (`airline_id`, `name`, `country`) VALUES
+(1, 'IndiGo', 'India'),
+(2, 'Emirates', 'UAE');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Airlines`
+--
+ALTER TABLE `Airlines`
+  ADD PRIMARY KEY (`airline_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
